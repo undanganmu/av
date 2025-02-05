2e782814588c1bf153b9083cbe9c3bef9ebbbafb
mfahnan <ahnanm06@gmail.com>
2025-02-05 11:34:27 +0700

    loading

---
+++ js/load.js
@@ -0,0 +1,8 @@
+``
+`javascript
+window.addEventListener('load', function() {
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+});
+`
+``
